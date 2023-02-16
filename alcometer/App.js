fd8055d06ef7312d598ef697a3f02ef9c4b3eadd
JSKarhu <92786584+JSKarhu@users.noreply.github.com>
2023-02-16 20:00:27 +0200

    made changes

--- alcometer/App.js
+++ alcometer/App.js
@@ -1,13 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { StyleSheet, Text, TextInput, Touchable, TouchableOpacity, View } from 'react-native';
+import { SafeAreaView, Text, TextInput, Touchable, TouchableOpacity, View } from 'react-native';
 import NumericInput from 'react-native-numeric-input';
-import { RadioButton, SegmentedButtons } from 'react-native-paper';
+import { Button, RadioButton, SegmentedButtons } from 'react-native-paper';
+import styles from './styles/styles';
 
-const sex = [
-  {genre: 'Male', weight: 0.7},
-  {genre: 'Female', weight: 0.6}
-];
 
 const radioStyle= {flexDirection: 'row', alignItems: 'center'};
 
@@ -15,38 +12,42 @@ export default function App() {
 
   const [bottles, setBottles] = useState();
   const [hours, setHours] = useState();
-  const [genre, setGenre] = useState();
+  const [weight, setWeight] = useState(0);
+  const [beer, setBeer] = useState(0);
+  const [time, setTime] = useState(0);
+  const [gender, setGender] = useState('male');
+  const [bloodAlcoholLevel, setBloodAlcoholLevel] = useState(0);
+
+  function calculateBloodAlcoholLevel() {
+    let r = 0.7;
+    if (gender === 'female') {
+      r = 0.6;
+    }
+    const bac = (beer * 12 * 0.05) / (weight * r);
+    const soberTime = (bac * 60) / 0.015;
+    setBloodAlcoholLevel(bac);
+    setSoberTime(soberTime);
+  }
 
   return (
-    <View style={styles.container}>
-      <Text>Alcometer</Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.header}>Alcometer</Text>
       <Text>Weight</Text>
-      <TextInput>Weight</TextInput>
+      <TextInput style={styles.textInput}
+      keyboardType='numeric'
+      placeholder='Weight (lbs)'
+      onChangeText={text => setWeight(text)}
+      />
       <Text>Bottles</Text>
-      <NumericInput onChange={b => setBottles(b)}></NumericInput>
+      <NumericInput onChange={b => setBeer(b)}></NumericInput>
       <Text>Hours</Text>
-      <NumericInput onChange={h => setHours(h)}></NumericInput>
-      <RadioButton.Group onValueChange={newValue => setGenre(sex.find(sex => sex.genre === newValue).weight)}>
-        {sex.map(sex =>
-          <View style={radioStyle} key={sex.genre} >
-            <RadioButton value={sex.genre}/>
-            <Text>{sex.genre}</Text>
-          </View>
-          )}
+      <NumericInput onChange={t => setTime(t)}></NumericInput>
+      <RadioButton.Group selectedValue={gender}
+      onValueChange={itemValue => setGender(itemValue)}>
+            <RadioButton.Android label='Male' value='male'  />
+            <RadioButton.Android label='Female' value='female'  />
       </RadioButton.Group>
-      <TouchableOpacity>
-          <Text>Calculate</Text>
-      </TouchableOpacity>
-
-    </View>
+      <Button style={styles.button} onPress={calculateBloodAlcoholLevel}>Calculate</Button>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ alcometer/styles/styles.js
@@ -0,0 +1,37 @@
+import { StyleSheet } from "react-native";
+
+
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        margin: 10,
+      },
+    button: {
+        width: 120,
+        backgroundColor: 'red',
+        padding: 5,
+        textAlign: 'center',
+        borderWidth: 1,
+        fontWeight: 'bold',
+        borderRadius: 5,
+        overflow: "hidden",
+    },
+    textInput: {
+        borderWidth:1, 
+        padding: 5,
+        width: 150
+    },
+    radio: {
+        color: 'black',
+        borderWidth: 1,
+    },
+    header: {
+        color: '#705000',
+        fontWeight: "bold",
+        fontSize: 30,
+        marginBottom: 10
+    },
+});
